7d0b3236bd7e829c2d2e8e9422e9e78395467238
HenraL <37299738+HenraL@users.noreply.github.com>
2020-09-15 22:29:53 +0200

    uploaded the files

---
+++ files/constantes.py
@@ -0,0 +1,172 @@
+"""Constantes du jeu de Labyrinthe Donkey Kong"""
+
+#Paramètres de la fenêtre
+# nombre_sprite_cote = 15
+nombre_sprite_cote = 100
+taille_sprite = 30
+# cote_fenetrex = 455
+# cote_fenetrey = 450
+cote_fenetrex=691
+cote_fenetrey=600
+
+#Personnalisation de la fenêtre
+titre_fenetre = "Wolf Escape"
+image_icone = "img/wolf_icon.png"
+imggiff="img/wolf_mainmenu.png"
+imggif="img/wolf_mainmenu2.png"
+
+#Listes des images du jeu
+image_accueil = "img/accueil.png"
+
+#creation du gardien
+mean_up="img/sprite/gardien/Gardien_haut.png"
+mean_down="img/sprite/gardien/Gardien_bas.png"
+mean_left="img/sprite/gardien/Gardien_Gauche.png"
+mean_right="img/sprite/gardien/Gardien_Droite.png"
+
+#fond
+image_fond = "img/background/fond.jpg"
+image_fond="img/background/fond.png"
+image_fond="img/background/fonds.png"
+
+#départ
+image_depart = "img/start.png"
+image_depart="img/start.png"
+# image_arrivee = "img/end.png"
+image_arrivee = "img/endd.png"
+image_arrivee = "img/endb.png"
+image_mur = "img/mur.png"
+
+#Création du perso
+ld="img/sprite/dk/dk_droite.png"
+ld="img/sprite/w/w_rigth.png"
+lg="img/sprite/dk/dk_gauche.png"
+lg="img/sprite/w/w_left.png"
+lh="img/sprite/dk/dk_haut.png"
+lh="img/sprite/w/w_up.png"
+lb="img/sprite/dk/dk_bas.png"
+lb="img/sprite/w/w_down.png"
+
+#création des flèches de direction pour le tuto
+aright="img/tut_image/aright.PNG"
+aleft="img/tut_image/aleft.PNG"
+aup="img/tut_image/aup.PNG"
+adown="img/tut_image/adown.PNG"
+
+#alphaet minuscule
+
+a="img/tut_image/alphabet/minuscule/ama.png"
+b="img/tut_image/alphabet/minuscule/bma.png"
+c="img/tut_image/alphabet/minuscule/cma.png"
+d="img/tut_image/alphabet/minuscule/dma.png"
+e="img/tut_image/alphabet/minuscule/ema.png"
+f="img/tut_image/alphabet/minuscule/fma.png"
+g="img/tut_image/alphabet/minuscule/gma.png"
+h="img/tut_image/alphabet/minuscule/hma.png"
+i="img/tut_image/alphabet/minuscule/ima.png"
+j="img/tut_image/alphabet/minuscule/jma.png"
+k="img/tut_image/alphabet/minuscule/kma.png"
+l="img/tut_image/alphabet/minuscule/lma.png"
+m="img/tut_image/alphabet/minuscule/mma.png"
+n="img/tut_image/alphabet/minuscule/nma.png"
+o="img/tut_image/alphabet/minuscule/oma.png"
+p="img/tut_image/alphabet/minuscule/pma.png"
+q="img/tut_image/alphabet/minuscule/qma.png"
+r="img/tut_image/alphabet/minuscule/rma.png"
+s="img/tut_image/alphabet/minuscule/sma.png"
+t="img/tut_image/alphabet/minuscule/tma.png"
+u="img/tut_image/alphabet/minuscule/uma.png"
+v="img/tut_image/alphabet/minuscule/vma.png"
+w="img/tut_image/alphabet/minuscule/wma.png"
+x="img/tut_image/alphabet/minuscule/xma.png"
+y="img/tut_image/alphabet/minuscule/yma.png"
+z="img/tut_image/alphabet/minuscule/zma.png"
+
+#Alphabet Majuscule
+A="img/tut_image/alphabet/majuscule/ama.png"
+B="img/tut_image/alphabet/majuscule/bma.png"
+C="img/tut_image/alphabet/majuscule/cma.png"
+D="img/tut_image/alphabet/majuscule/dma.png"
+E="img/tut_image/alphabet/majuscule/ema.png"
+F="img/tut_image/alphabet/majuscule/fma.png"
+G="img/tut_image/alphabet/majuscule/gma.png"
+H="img/tut_image/alphabet/majuscule/hma.png"
+I="img/tut_image/alphabet/majuscule/ima.png"
+J="img/tut_image/alphabet/majuscule/jma.png"
+K="img/tut_image/alphabet/majuscule/kma.png"
+L="img/tut_image/alphabet/majuscule/lma.png"
+M="img/tut_image/alphabet/majuscule/mma.png"
+N="img/tut_image/alphabet/majuscule/nma.png"
+O="img/tut_image/alphabet/majuscule/oma.png"
+P="img/tut_image/alphabet/majuscule/pma.png"
+Q="img/tut_image/alphabet/majuscule/qma.png"
+R="img/tut_image/alphabet/majuscule/rma.png"
+S="img/tut_image/alphabet/majuscule/sma.png"
+T="img/tut_image/alphabet/majuscule/tma.png"
+U="img/tut_image/alphabet/majuscule/uma.png"
+V="img/tut_image/alphabet/majuscule/vma.png"
+W="img/tut_image/alphabet/majuscule/wma.png"
+X="img/tut_image/alphabet/majuscule/xma.png"
+Y="img/tut_image/alphabet/majuscule/yma.png"
+Z="img/tut_image/alphabet/majuscule/zma.png"
+
+# img/tut_image/1.PNG
+# img/tut_image/2.PNG
+# img/tut_image/3.PNG
+# img/tut_image/4.PNG
+# img/tut_image/5.PNG
+# img/tut_image/6.PNG
+# img/tut_image/7.PNG
+# img/tut_image/8.PNG
+# img/tut_image/9.PNG
+# img/tut_image/10.PNG
+# img/tut_image/11.PNG
+# img/tut_image/12.PNG
+# img/tut_image/13.PNG
+# img/tut_image/14.PNG
+# img/tut_image/15.PNG
+# img/tut_image/16.PNG
+# img/tut_image/17.PNG
+# img/tut_image/18.PNG
+# img/tut_image/19.PNG
+# img/tut_image/20.PNG
+# img/tut_image/21.PNG
+# img/tut_image/22.PNG
+# img/tut_image/23.PNG
+# img/tut_image/24.PNG
+# img/tut_image/25.PNG
+# img/tut_image/26.PNG
+# img/tut_image/27.PNG
+# img/tut_image/28.PNG
+# img/tut_image/29.PNG
+# img/tut_image/30.PNG
+# img/tut_image/31.PNG
+# img/tut_image/32.PNG
+# img/tut_image/33.PNG
+# img/tut_image/34.PNG
+# img/tut_image/35.PNG
+# img/tut_image/36.PNG
+# img/tut_image/37.PNG
+# img/tut_image/38.PNG
+# img/tut_image/39.PNG
+# img/tut_image/40.PNG
+# img/tut_image/41.PNG
+# img/tut_image/42.PNG
+# img/tut_image/43.PNG
+# img/tut_image/44.PNG
+# img/tut_image/45.PNG
+# img/tut_image/46.PNG
+# img/tut_image/47.PNG
+# img/tut_image/48.PNG
+# img/tut_image/49.PNG
+# img/tut_image/50.PNG
+# img/tut_image/51.PNG
+# img/tut_image/52.PNG
+# img/tut_image/53.PNG
+# img/tut_image/54.PNG
+# img/tut_image/55.PNG
+# img/tut_image/56.PNG
+# img/tut_image/57.PNG
+# img/tut_image/58.PNG
+# img/tut_image/59.PNG
+# img/tut_image/0.PNG
