235354989ef7aacabba6aa109e719f0f7394a393
HenraL <37299738+HenraL@users.noreply.github.com>
2020-10-24 14:59:08 +0200

    delt with the sprite appending program and creating a level creator program

---
+++ files/creation_niveau/create level/Level_creator.py
@@ -0,0 +1,204 @@
+# Sprite file link = https://repl.it/@HanraLatalliar/Sprites#Spritelist.py
+from tkinter import *
+from time import sleep
+answer=""
+class tk_windows:
+    def __init__(twin,mainwinchoice):
+        twin.mainwinchoice=mainwinchoice
+    # def main_menu(twin):
+    #     continue
+    def QuestionYN(sentence1,sentence2,answer1,answer2):
+        def Yes():
+            fenetre.destroy()
+            answer="yes"
+            return answer
+        def No():
+            answer="no"
+            fenetre.destroy()
+            return answer
+        fenetre = Tk()
+        Frame1 = Frame(fenetre, borderwidth=2, relief=FLAT)
+        Frame1.pack(side=TOP, padx=10, pady=10)
+        FrameButt=Frame(fenetre, borderwidth=2, relief=FLAT)
+        FrameButt.pack(side=TOP, padx=0, pady=10)
+
+        label = Label(Frame1, text="{sentence1}")
+        label.pack()
+        label = Label(Frame1, text="{sentence2}")
+        label.pack()
+        bouton=Button(FrameButt, text="{answer1}", command=Yes)
+        bouton.pack(side=RIGHT, padx=5)
+        bouton=Button(FrameButt, text="{answer2}", command=No)
+        bouton.pack(side=RIGHT, padx=5)
+        fenetre.mainloop()
+    def watermarkf(s):
+        print("\ncreated by Henry Letellier\n ._____. \n |.   .| \n |  |  | \n |\___/| \n |_____| ")
+        sleep(s)
+import pygame, os, sys, platform
+from pygame.locals import *
+from pygame.mixer import *
+from pygame.display import *
+
+from webbrowser import open_new
+
+given=False
+while given==False:
+    author=input("(This is for the side info in the level file (knowing who did wich))\nHow do you wish to be called? ")
+    if author=="" or author==" " or author=="  " or author=="   ":
+        given=False
+        print("Please enter your name, this is just to put a name on the author of the level")
+    else:
+        given=True
+
+pygame.init()
+
+Computer_type=platform.system()
+CT=str(Computer_type)
+# mac: https://youtu.be/j3yH6FfD_Wk
+# pc: https://github.com/HenraL/Install_pygame/tree/master/files
+# linux: https://www.youtube.com/results?search_query=how+to+install+pip+on+linux
+
+try:
+    import requests
+except:
+    try:
+        os.system("pip install requests")
+    except:
+        try:
+            try:
+                os.system("py -m pip install pip")
+            except:
+                os.system("py -m pip install --upgrade pip")
+        except:
+            print("If this is your first time using this program, please consider running it with admin rights.")
+            if CT=='Windows':
+                piplink="https://github.com/HenraL/Install_pygame/tree/master/files"
+            elif CT=='Linux':
+                piplink="https://www.youtube.com/results?search_query=how+to+install+pip+on+linux"
+            else:
+                piplink="https://youtu.be/j3yH6FfD_Wk"
+            tk_windows.QuestionYN("If this is already the case, please consider installing the 'pip' module to enable the program to download and import the 'requests' library.","Do you wish to open the link to the pip installing file? (link: {}) [(y)es/(n)o]".format(piplink),"Yes","No")
+            askpip=answer
+            if askpip=="y" or askpip=="Y":
+                open_new(piplink)
+            else:
+                print()
+
+class dealsys:
+    def __init__(DS,currentd,namespf,filenamebool):
+        # global DS
+        DS.cd=currentd
+        DS.spf=namespf
+        DS.fbool=filenamebool
+    def pause():
+        pause=input("Please press enter to continue...")
+    def getCurrent():
+        global currentd
+        currentd=os.getcwd()
+        if platform.system()=="Windows":
+           currentd+="\\"
+        else:
+            currentd+="/"
+        return currentd
+    def getName(currentd,filename):
+        global filenamebool
+        if os.path.exists("{}{}".format(currentd,filename))==True:
+            filenamebool=True
+        else:
+            filenamebool=False
+        return filenamebool
+
+dealsys.getCurrent()
+dealsys.getName(currentd,"Spritelist.py")
+print(filenamebool)
+if filenamebool==False:
+    # try:
+    #     get file.
+    # except:
+    #   We have not succeded in downloading the file ... ples visit ... to download it
+    print()
+else:
+    print("File ... succesfully downloaded.")
+contiinue=True
+while contiinue==True:
+# tk_window.main_menu()
+# tw=tk_window
+# if tw=="s": #(show)
+#   tw.get_level()
+# elif tw=="c": #(create)
+#   tw.get_level_file_name()
+#   tw.get_spritelist()
+#   tw.display_spritelist_collum_scroll() (or if this blocks prog, tw.display_spritelist_collum_scroll_non_blocker()
+#   level_list=[]
+#   while still_in_edit==True:
+#       game.display_pygame_widow(current)
+#       height=20
+#       length=23
+#       while line_still_in_edditing==True:
+#           for sprite_line in range(heigth-1):
+#               level_list_icon=[]
+#               for sprite_icon in range(length-1):
+#                   SpriteSpriteGiven=False
+#                   while SpriteSpriteGiven==False:
+#                       Sprite=input("Entrez le numéro du sprite que vous souhaitez ajouter sur la ligne n°{sprite_line} à la case n°{sprite_icon}")
+#                       for checkSprite in range(len(SpriteList)):
+#                           if Spritelist[checkSprite]["ID"]==Sprite:
+#                               SpriteListExist=True
+#                               break
+#                           else:
+#                               if checkSprite==len(SpriteList):
+#                                   SpriteListExist=False
+#                                   print(Le Sprite que vous avez entré n'existe pas, vérifiez sont Identifiant et retentez à nouveau.)
+#                       if SpriteListExist==True:
+#                           GetBon=input("Le srite {Sprite} du nom de {SpriteId} dont l'image se nome {SpriteImage} vous convient-il? [(o)ui/(n)on]")
+#                           if GetBon=="o":
+#                               level_list_icon.append("{}".format(str(Sprite)))
+#                               temp_level_list[sprite_icon]=level_list_icon
+#                               game.display_pygame_window(current)
+#                           else:
+#                               SpritelistExist=False
+#                       else:
+#                           print()
+#               level_list.append(level_list_icon)
+#               temp_level_list=level_list
+#               game.display_pygame_window(current)
+#   dealsys.write_level(level_list) ((  total_to_write=""
+#                                       for line in range(len(list)):
+#                                           for icons in range(len(list[line])):
+#                                               total_to_write+="{icons}|"
+#                                           total_to_write+="\n"
+#                                       total_to_write+="\n#Level Created by '{author}' at {DATETIMEDATE,DAY,MONTH,YEAR}\n#This level was created with the leve_creator.\n#The program level_creator.py was coded, thougth and designed by Henry Letellier\n#If you liked this level\n#You can inform its creator by posting on the discord server in the following way:\n#Goto the room #level_feedback\n#write your message\n#Please strucutre your message the following way:\n#name of the level file (or of the title of the window)\n#what you thought about the level\n#(optional): your name."
+#                                       f=open(filename,"w")
+#                                       f.write(total_to_write)
+#                                       f.close()
+#                                       ))
+# elif tw=="t": (test)
+#   tw.disclamer_on_partialy_finished_levels()
+#   game.play(current_list)
+# elif tw=="m": (modify)
+#   tw.show_level_list_and_ask_which_level_to_open_to_edit()
+#   dealsys.open_level_to_edit()
+#   dealsys.show_level_with_sprite_matrix()
+#   tw.ask_line_to_mod()
+#   tw.ask_sprite_to_mod_in_line()
+#   sprite_chosen=False
+#   while sprite_chosen==False:
+#       new_sprite_get=input("Enter the number corresonding to the new sprite: ")
+#       dealsys.check_list()
+#       if check_list==True:
+#           level_edit[line][sprite]=new_sprite_get
+#           sprite_chosen=True
+#       else:
+#           print("The Sprite that you chose does not exist.\nPlease enter a new one.\nYou have entered {new_sprite_get}.")
+#           sprite_chosen=False
+# elif tw=="r": (refresh)
+#   try:
+#       dealsys.refresh(list)
+#       from Spritelist import *
+#       print("The Spritelist has been refreshed")
+#   except:
+#       print("The Spritelist coudln't be refreshed due to an unknown error.\nPlease check your internet connection and try again (or running this program whith admin rights).")
+# elif tw=="q": (quit)
+#   print("Thank you for using this program.\nSee you next time.\nThis program was created by Henry Letellier.")
+#   tw.watermarkf(2)
+#   dealsys.pause()
---
+++ files/creation_niveau/create level/constantes.py
@@ -0,0 +1,235 @@
+"""Constantes du jeu de Labyrinthe Donkey Kong"""
+
+#ingame booleen variables
+CREDIT=False
+# exitSpaceGame=False
+
+#Paramètres de la fenêtre
+# nombre_sprite_cote = 15
+nombre_sprite_cote = 100
+taille_sprite = 30
+# cote_fenetrex = 455
+# cote_fenetrey = 450
+cote_fenetrex=691
+cote_fenetrey=600
+
+#Personnalisation de la fenêtre
+titre_fenetre = "Wolf Escape"
+image_icone = "img/ingame/wolf_icon.png"
+image_main1="img/launch_load/menu_anim/accueil.png"
+image_main2="img/launch_load/menu_anim/accueil_static_2.png"
+
+#annimation du jeux
+##Annimation du menu démarer
+mainMenuPlayed=True#False#True #False #True #False #True #False#True
+frameMainMenuWait=0.5
+image_accueil1="img/launch_load/start_load/stages_stage_1.png"
+image_accueil2="img/launch_load/start_load/stages_stage_2.png"
+image_accueil3="img/launch_load/start_load/stages_stage_3.png"
+image_accueil4="img/launch_load/start_load/stages_stage_4.png"
+image_accueil5="img/launch_load/start_load/stages_stage_5.png"
+image_accueil6="img/launch_load/start_load/stages_stage_6.png"
+image_accueil7="img/launch_load/start_load/stages_stage_7.png"
+image_accueil8="img/launch_load/start_load/stages_stage_8.png"
+image_accueil9="img/launch_load/start_load/stages_stage_9.png"
+image_accueil10="img/launch_load/start_load/stages_stage_10.png"
+image_accueil11="img/launch_load/start_load/stages_stage_11.png"
+image_accueil12="img/launch_load/start_load/stages_stage_12.png"
+image_accueil13="img/launch_load/start_load/stages_stage_13.png"
+image_accueil14="img/launch_load/start_load/stages_stage_14.png"
+image_accueil15="img/launch_load/start_load/stages_stage_15.png"
+
+load_images=[image_accueil1,image_accueil2,image_accueil3,image_accueil4,image_accueil5,image_accueil6,image_accueil7,image_accueil8,image_accueil9,image_accueil10,image_accueil11,image_accueil12,image_accueil13,image_accueil14,image_accueil15,image_main1,image_main2]
+
+
+#Listes des images du jeu
+image_accueil = "img/ingame/accueil.png"
+
+#creation du gardien
+img_mean_up="img/sprite/gardien/Gardien_haut.png"
+img_mean_down="img/sprite/gardien/Gardien_bas.png"
+img_mean_left="img/sprite/gardien/Gardien_Gauche.png"
+img_mean_right="img/sprite/gardien/Gardien_Droite.png"
+
+#fond
+image_fond = "img/background/fond.jpg"
+image_fond="img/background/fond.png"
+image_fond="img/background/fonds.png"
+image_fond_credits="img/Credits/end - Copie.PNG"
+
+#départ
+image_depart = "img/ingame/start.png"
+image_depart="img/ingame/start.png"
+# image_arrivee = "img/end/end.png"
+image_arrivee = "img/end/endd.png"
+image_arrivee = "img/end/endb.png"
+image_arrivee="img/end/endv.png"
+image_mur = "img/ingame/mur.png"
+image_arrivee_fam="img/sprite/famille/w_fam.png"
+
+
+#Création du perso
+ld="img/sprite/dk/dk_droite.png"
+ld="img/sprite/w/w_rigth.png"
+lg="img/sprite/dk/dk_gauche.png"
+lg="img/sprite/w/w_left.png"
+lh="img/sprite/dk/dk_haut.png"
+lh="img/sprite/w/w_up.png"
+lb="img/sprite/dk/dk_bas.png"
+lb="img/sprite/w/w_down.png"
+
+#création des flèches de direction pour le tuto
+img_aright="img/tut_image/aright.PNG"
+img_aleft="img/tut_image/aleft.PNG"
+img_aup="img/tut_image/aup.PNG"
+img_adown="img/tut_image/adown.PNG"
+
+#Crédits
+image_credits_fond="img/Credits/end.png"
+#Crédits_image_gané
+image_credits_gagne="img/sprite/famille/famille_loup_You_have_won.png"
+#Crédits_image_w_Irina
+image_credits_w_Irina="img/sprite/famille/Mere et petits.png"
+#Crédits_image_w_Henry
+image_credits_w_Henry="img/sprite/famille/w_fam.png"
+#Crédits_image_l_Irina
+image_credits_l_Irina="img/sprite/famille/Irina30x30.png"
+#Crédits_image_l_Henry
+image_credits_l_Henry="img/sprite/famille/Henry30x30.png"
+
+#numéros
+image_zero="img/tut_image/numbers/0.png"
+image_one="img/tut_image/numbers/1.png"
+image_two="img/tut_image/numbers/2.png"
+image_three="img/tut_image/numbers/3.png"
+image_four="img/tut_image/numbers/4.png"
+image_five="img/tut_image/numbers/5.png"
+image_six="img/tut_image/numbers/6.png"
+image_seven="img/tut_image/numbers/7.png"
+image_eight="img/tut_image/numbers/8.png"
+image_nine="img/tut_image/numbers/9.png"
+
+#monaie
+image_dollar="img/tut_image/currency/dollard.png"
+image_pound="img/tut_image/currency/pound.png"
+image_euro="img/tut_image/currency/euro.png"
+image_yen="img/tut_image/currency/yen.png"
+image_whan="img/tut_image/currency/wan.png"
+
+#alphabet minuscule
+
+leta="img/tut_image/alphabet/minuscule/ami.png"
+letb="img/tut_image/alphabet/minuscule/bmi.png"
+letc="img/tut_image/alphabet/minuscule/cmi.png"
+letd="img/tut_image/alphabet/minuscule/dmi.png"
+lete="img/tut_image/alphabet/minuscule/emi.png"
+letf="img/tut_image/alphabet/minuscule/fmi.png"
+letg="img/tut_image/alphabet/minuscule/gmi.png"
+leth="img/tut_image/alphabet/minuscule/hmi.png"
+leti="img/tut_image/alphabet/minuscule/imi.png"
+letj="img/tut_image/alphabet/minuscule/jmi.png"
+letk="img/tut_image/alphabet/minuscule/kmi.png"
+letl="img/tut_image/alphabet/minuscule/lmi.png"
+letm="img/tut_image/alphabet/minuscule/mmi.PNG"
+letn="img/tut_image/alphabet/minuscule/nmi.png"
+leto="img/tut_image/alphabet/minuscule/omi.png"
+letp="img/tut_image/alphabet/minuscule/pmi.png"
+letq="img/tut_image/alphabet/minuscule/qmi.png"
+letr="img/tut_image/alphabet/minuscule/rmi.png"
+lets="img/tut_image/alphabet/minuscule/smi.png"
+lett="img/tut_image/alphabet/minuscule/tmi.png"
+letu="img/tut_image/alphabet/minuscule/umi.png"
+letv="img/tut_image/alphabet/minuscule/vmi.png"
+letw="img/tut_image/alphabet/minuscule/wmi.png"
+letx="img/tut_image/alphabet/minuscule/xmi.PNG"
+lety="img/tut_image/alphabet/minuscule/ymi.PNG"
+letz="img/tut_image/alphabet/minuscule/zmi.png"
+
+#Alphabet Majuscule
+letA="img/tut_image/alphabet/majuscule/ama.png"
+letB="img/tut_image/alphabet/majuscule/bma.png"
+letC="img/tut_image/alphabet/majuscule/cma.png"
+letD="img/tut_image/alphabet/majuscule/dma.png"
+letE="img/tut_image/alphabet/majuscule/ema.png"
+letF="img/tut_image/alphabet/majuscule/fma.png"
+letG="img/tut_image/alphabet/majuscule/gma.png"
+letH="img/tut_image/alphabet/majuscule/hma.png"
+letI="img/tut_image/alphabet/majuscule/ima.png"
+letJ="img/tut_image/alphabet/majuscule/jma.png"
+letK="img/tut_image/alphabet/majuscule/kma.png"
+letL="img/tut_image/alphabet/majuscule/lma.png"
+letM="img/tut_image/alphabet/majuscule/mma.png"
+letN="img/tut_image/alphabet/majuscule/nma.png"
+letO="img/tut_image/alphabet/majuscule/oma.png"
+letP="img/tut_image/alphabet/majuscule/pma.png"
+letQ="img/tut_image/alphabet/majuscule/qma.png"
+letR="img/tut_image/alphabet/majuscule/rma.png"
+letS="img/tut_image/alphabet/majuscule/sma.png"
+letT="img/tut_image/alphabet/majuscule/tma.png"
+letU="img/tut_image/alphabet/majuscule/uma.png"
+letV="img/tut_image/alphabet/majuscule/vma.png"
+letW="img/tut_image/alphabet/majuscule/wma.png"
+letX="img/tut_image/alphabet/majuscule/xma.png"
+letY="img/tut_image/alphabet/majuscule/yma.png"
+letZ="img/tut_image/alphabet/majuscule/zma.png"
+
+# img/tut_image/1.PNG
+# img/tut_image/2.PNG
+# img/tut_image/3.PNG
+# img/tut_image/4.PNG
+# img/tut_image/5.PNG
+# img/tut_image/6.PNG
+# img/tut_image/7.PNG
+# img/tut_image/8.PNG
+# img/tut_image/9.PNG
+# img/tut_image/10.PNG
+# img/tut_image/11.PNG
+# img/tut_image/12.PNG
+# img/tut_image/13.PNG
+# img/tut_image/14.PNG
+# img/tut_image/15.PNG
+# img/tut_image/16.PNG
+# img/tut_image/17.PNG
+# img/tut_image/18.PNG
+# img/tut_image/19.PNG
+# img/tut_image/20.PNG
+# img/tut_image/21.PNG
+# img/tut_image/22.PNG
+# img/tut_image/23.PNG
+# img/tut_image/24.PNG
+# img/tut_image/25.PNG
+# img/tut_image/26.PNG
+# img/tut_image/27.PNG
+# img/tut_image/28.PNG
+# img/tut_image/29.PNG
+# img/tut_image/30.PNG
+# img/tut_image/31.PNG
+# img/tut_image/32.PNG
+# img/tut_image/33.PNG
+# img/tut_image/34.PNG
+# img/tut_image/35.PNG
+# img/tut_image/36.PNG
+# img/tut_image/37.PNG
+# img/tut_image/38.PNG
+# img/tut_image/39.PNG
+# img/tut_image/40.PNG
+# img/tut_image/41.PNG
+# img/tut_image/42.PNG
+# img/tut_image/43.PNG
+# img/tut_image/44.PNG
+# img/tut_image/45.PNG
+# img/tut_image/46.PNG
+# img/tut_image/47.PNG
+# img/tut_image/48.PNG
+# img/tut_image/49.PNG
+# img/tut_image/50.PNG
+# img/tut_image/51.PNG
+# img/tut_image/52.PNG
+# img/tut_image/53.PNG
+# img/tut_image/54.PNG
+# img/tut_image/55.PNG
+# img/tut_image/56.PNG
+# img/tut_image/57.PNG
+# img/tut_image/58.PNG
+# img/tut_image/59.PNG
+# img/tut_image/0.PNG
